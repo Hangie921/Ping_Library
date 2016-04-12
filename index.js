c9d609ec2e27db1189768b564767c05195351c79
pohua920 <pohua920@gmail.com>
2016-04-12 16:44:24 +0800

    更新package.json ，lib 以index.js 為main 修正index.js export 方法

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 var gengod = require('./common/generate/god');
-module.exports = require('./interface/session.js');
+var main = require('./interface/session.js');
+main.Users =  require('./bean/users');
+main.Roles =  require('./bean/roles');
+main.Functions =  require('./bean/function');
+module.exports = main;
