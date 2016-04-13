8aea96df65f8c09edd198f4655bfefe88b60a2dd
pohua920 <pohua920@gmail.com>
2016-04-13 11:14:28 +0800

    (user,role,funtion) service export

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ var gengod = require('./common/generate/god');
 var main = require('./interface/session.js');
 main.User =  require('./bean/users');
 main.Role =  require('./bean/roles');
-main.Fun =  require('./bean/function');
+main.Func =  require('./bean/function');
+
+main.UserService = require('./service/userService');
+main.RoleService = require('./service/roleService');
+main.FunctioService = require('./service/functionService');
 module.exports = main;
