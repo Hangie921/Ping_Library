0b0754cffc8ddaea4901ba74129e0e601efaf2dd
pohua920 <pohua920@gmail.com>
2016-04-13 14:19:08 +0800

    export response obj

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var gengod = require('./common/generate/god');
 var main = require('./interface/session.js');
-main.User =  require('./bean/users');
-main.Role =  require('./bean/roles');
-main.Func =  require('./bean/function');
+main.User = require('./bean/users');
+main.Role = require('./bean/roles');
+main.Func = require('./bean/function');
 
 main.UserService = require('./service/userService');
 main.RoleService = require('./service/roleService');
 main.FunctioService = require('./service/functionService');
+
+main.response = require('./common/response');
 module.exports = main;
