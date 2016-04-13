2f0d9ae788c7e10528b95a6b68a40e18377106ea
pohua920 <pohua920@gmail.com>
2016-04-13 10:40:55 +0800

    物件export 複數改單數  roles=>role,users=>user,function=>fun

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var gengod = require('./common/generate/god');
 var main = require('./interface/session.js');
-main.Users =  require('./bean/users');
-main.Roles =  require('./bean/roles');
-main.Functions =  require('./bean/function');
+main.User =  require('./bean/users');
+main.Role =  require('./bean/roles');
+main.Fun =  require('./bean/function');
 module.exports = main;
