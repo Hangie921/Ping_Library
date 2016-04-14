fe3df59e6cd88838943e779afb235dadaffe5e6e
pohua920 <pohua920@gmail.com>
2016-04-14 13:03:31 +0800

    sessionService 命名修正, 流程修正

--- service/sessionService.js
+++ service/sessionService.js
@@ -6,31 +6,39 @@ var usersobj = require('../bean/users');
 var response = require('../common/response');
 
 //--PUBLIC FUNCTION---
-var session_getUser = function(req,res,obj,callback){
-	userService.getUser(obj,function (data) {
-		req.session.user = data.values;
-		callback(data);
-	});
+var session_login = function(req, res, obj, callback) {
+    userService.getUser(obj, function(data) {
+        req.session.user = data.code === 200 ? data.values : null;
+        try {
+            delete data.values.pwd;
+        } catch (e) {}
+        callback(data);
+    });
 }
-exports.getUser = session_getUser;
 
-var session_getUserSession = function(req,res,callback){
-	try{
-		callback(response.obj(response.codeEnum.OK,req.session.user));
-	}catch(e){
-	  	callback(response.obj(response.codeEnum.Not_Found,null));
-	}
+var session_getUserSession = function(req, res, callback) {
+    try {
+        if (req.session.user === null) {
+            callback(response.obj(response.codeEnum.Not_Found, null));
+        } else {
+            callback(response.obj(response.codeEnum.OK, req.session.user));
+        }
+    } catch (e) {
+        callback(response.obj(response.codeEnum.Not_Found, null));
+    }
 }
+
+
+var session_cleanUserSession = function(req, res, callback) {
+    try {
+        req.session.user = null;
+        callback(response.obj(response.codeEnum.OK, req.session.user));
+    } catch (e) {
+        callback(response.obj(response.codeEnum.Not_Found, null));
+    }
+}
+
+
+exports.login = session_login;
 exports.getUserSession = session_getUserSession;
-
-
-var session_cleanUserSession = function(req,res,callback){
-	try{
-		req.session.user = null;
-		callback(response.obj(response.codeEnum.OK,req.session.user));
-	}catch(e){
-	  	callback(response.obj(response.codeEnum.Not_Found,null));
-	}
-}
 exports.cleanUserSession = session_cleanUserSession;
-
